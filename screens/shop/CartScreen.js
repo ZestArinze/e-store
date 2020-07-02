b89e079ac2213b50e4c990ee89cb6cffcb665dd1
Arinze Anakor <arinzeanakor@gmail.com>
2020-07-02 11:35:58 +0100

    chore: initial project setup

---
+++ screens/shop/CartScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+const CartScreen = (props) => {};
+
+const styles = StyleSheet.create({});
+
+export default CartScreen;
---
+++ screens/shop/ProductDetailsScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+const ProductDatailsScreen = (props) => {};
+
+const styles = StyleSheet.create({});
+
+export default ProductDatailsScreen;
