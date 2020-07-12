a7d722ea7706958eb2911e2fc91e96c62418ef13
Arinze Anakor <arinzeanakor@gmail.com>
2020-07-12 06:13:17 +0100

    same commit

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { createAppContainer } from "react-navigation";
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import { createDrawerNavigator } from "react-navigation-drawer";
 import { Platform } from "react-native";
---
+++ screens/user/AuthScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, ScrollView } from "react-native";
+
+import Input from "../../components/ui/Input";
+
+const AuthScreen = (props) => {
+  return <ScrollView></ScrollView>;
+};
+
+const styles = StyleSheet.create({});
+
+export default AuthScreen;
